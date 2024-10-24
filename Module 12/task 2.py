72d9c08343e4ccab2b4bcb3180ad3b70f49e2b0b
loooonnngg <long.vo2@metropolia.fi>
2024-10-24 11:18:16 +0300

    hide API key of weathermap in module12, task 2

--- Module 12/task 2.py
+++ Module 12/task 2.py
@@ -1,6 +1,7 @@
 import requests
+API_key=input("enter your weathermap API key: ")
 city=input("Enter city name: ")
-request="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8d7fae354e6f647175e5b2b957a0f610"
+request="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_key
 response = requests.get(request)
 print(response.json()["weather"][0]["description"])
 print(round(response.json()['main']['temp']-273.15,2),"Celsius")
